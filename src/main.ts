5c291d5908de97c550812269639300d2df647d40
mdamdinb <152232289+mdamdinb@users.noreply.github.com>
2025-10-16 20:58:01 +0000

    step 1 comepleted: Added App Title using <h1>, added a cnavas using css styling to add a border and drop shadow. Step 2 completed: Implemented a drawing tool onto the canvas using mouse event listeners. Implemented a clear button to clear the canvas by making it white again. "co-coded with Brody Vance; blvance@ucsc.edu" for section assignment.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,55 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <h1>D2 Assignment</h1>
+  <canvas id="myCanvas" width="256" height="256"></canvas>
+  <button id = "clrButton">clear</button>
+  
 `;
+`cgetElementById("myCanvas");
+
+const myCanvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+const ctx = myCanvas.getContext("2d")!;
+const clrButton = document.getElementById("clrButton") as HTMLButtonElement;
+
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+clrButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+});
+
+myCanvas.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+myCanvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    drawLine(ctx, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+myCanvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+function drawLine(
+  ctx: CanvasRenderingContext2D,
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number
+) {
+  ctx.beginPath();
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 1;
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+  ctx.closePath();
+}
