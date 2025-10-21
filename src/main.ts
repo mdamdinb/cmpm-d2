7154d2d59920d1b44933298dc45675cf62f7a9c2
mdamdinb <152232289+mdamdinb@users.noreply.github.com>
2025-10-21 23:15:21 +0000

    step 4 completed. Added undo/redo buttons. Implemented redoStack array. Implemented undo/redo buttton click handlers. Changed clear to use splice. Modified mousedown to initialize empty array, push to strokes, clear redo stack, then add first point. Clear redo stack when theres a new stroke.

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ document.body.innerHTML = `
   <h1>D2 Assignment</h1>
   <canvas id="myCanvas" width="256" height="256"></canvas>
   <button id = "clrButton">clear</button>
+  <button id="undoButton">undo</button>
+  <button id="redoButton">redo</button>
   
 `;
 
 const myCanvas = document.getElementById("myCanvas") as HTMLCanvasElement;
 const ctx = myCanvas.getContext("2d")!;
 const clrButton = document.getElementById("clrButton") as HTMLButtonElement;
+const undoButton = document.getElementById("undoButton") as HTMLButtonElement;
+const redoButton = document.getElementById("redoButton") as HTMLButtonElement;
 
 interface Point {
   x: number;
@@ -18,16 +22,34 @@ interface Point {
 
 type Stroke = Point[];
 const strokes: Stroke[] = [];
+const redoStack: Stroke[] = [];
 let currentStroke: Stroke | null = null;
 
 clrButton.addEventListener("click", () => {
-  strokes.length = 0; //clears the data
-  dispatchDrawingChanged(); //triggers redraw
+  strokes.splice(0, strokes.length);
+  dispatchDrawingChanged();
+});
+
+undoButton.addEventListener("click", () => {
+  if (strokes.length > 0) {
+    redoStack.push(strokes.pop()!);
+    dispatchDrawingChanged();
+  }
+});
+
+redoButton.addEventListener("click", () => {
+  if (redoStack.length > 0) {
+    strokes.push(redoStack.pop()!);
+    dispatchDrawingChanged();
+  }
 });
 
 myCanvas.addEventListener("mousedown", (e) => {
-  currentStroke = [{ x: e.offsetX, y: e.offsetY }];
+  currentStroke = [];
   strokes.push(currentStroke);
+  redoStack.splice(0, redoStack.length); //clear redo stack on new stroke
+  currentStroke.push({ x: e.offsetX, y: e.offsetY });
+
   dispatchDrawingChanged();
 });
 
